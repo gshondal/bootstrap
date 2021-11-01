c3478fc314124609c4ddb93068b5a9fe27f85242
SH Groom <58953578+gshondal@users.noreply.github.com>
2021-11-01 05:27:54 -0400

    Create al2-terraform.sh

---
+++ aws-ec2/al2-terraform.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum update -y
+
+# https://learn.hashicorp.com/tutorials/terraform/install-cli?in=terraform/aws-get-started
+yum install yum-utils -y
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum install terraform -y
+
+su ec2-user -c '
+cd; touch ~/.bashrc;
+terraform -install-autocomplete
+'
